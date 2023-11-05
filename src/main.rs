4084f8554eb2362ee2c38fee2549c6006a3f4cb2
Lars Riel <larsriel@gmail.com>
2023-11-05 19:14:06 +0100

    updated default environment

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ mod server;
 extern crate unnamed_cms_lib;
 
 fn main() {
+    // TODO: Remove this when deploying
     env::set_var("RUST_BACKTRACE", "full");
     env::set_var("SURREAL_URL", "ws://localhost:8000");
     env::set_var("SURREAL_USER", "root");
     env::set_var("SURREAL_PASS", "root");
+    
+    if env::var("JWT_SECRET").is_err() {
+        env::set_var("JWT_SECRET", "secret");
+    }
 
     server::main();
 }
